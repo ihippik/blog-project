269bdbfacdc849b487ef76547ad23ec2db78b571
Konstantin Makarov <hippik80@gmail.com>
2025-11-30 18:48:35 +0300

    Add proto

---
+++ blog-client/build.rs
@@ -0,0 +1,13 @@
+// should be run before cargo build
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("cargo:rerun-if-changed=proto/blog.proto");
+    
+    tonic_build::configure()
+        .build_server(true)
+        .build_client(true)
+        .compile(
+            &["proto/blog.proto"],
+            &["proto"],
+        )?;
+    Ok(())
+}
